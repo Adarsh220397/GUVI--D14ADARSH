adc77dbaa13f599b80c3ba899e01d5e0073c30d8
Adarsh220397 <79439872+Adarsh220397@users.noreply.github.com>
2021-09-26 16:34:42 +0530

    Students  name list and marks of 5 subjects
    Calculated total and average for subjects of 5 students , the value has been entered via user input.

---
+++ class assignment .py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+a , b, c, d, e= list(map(str,input("Enter student name : ").split()))
+
+print(f"student {a}")
+m1 = int(input("Mark of subject 1 : "))
+m2 = int(input("Mark of subject 2 : "))
+m3 = int(input("Mark of subject 3 : "))
+m4 = int(input("Mark of subject 4 : "))
+m5 = int(input("Mark of subject 5 : "))
+
+total_1 = m1+ m2+ m3+ m4+ m5
+print(total_1)
+
+avg_1 = total/5
+print(avg_1)
+print()
+print(f'student {b}')
+m1 = int(input("Mark of subject 1 : "))
+m2 = int(input("Mark of subject 2 : "))
+m3 = int(input("Mark of subject 3 : "))
+m4 = int(input("Mark of subject 4 : "))
+m5 = int(input("Mark of subject 5 : "))
+
+total_2 = m1+ m2+ m3+ m4+ m5
+print(total_2)
+
+avg_2 = total_2/5
+print(avg_2)
+print()
+print(f'student {c}')
+
+m1 = int(input("Mark of subject 1 : "))
+m2 = int(input("Mark of subject 2 : "))
+m3 = int(input("Mark of subject 3 : "))
+m4 = int(input("Mark of subject 4 : "))
+m5 = int(input("Mark of subject 5 : "))
+
+total_3 = m1+ m2+ m3+ m4+ m5
+print(total_2)
+
+avg_3 = total_3/5
+print(avg_3)
+print()
+print(f'student {d}')
+m1 = int(input("Mark of subject 1 : "))
+m2 = int(input("Mark of subject 2 : "))
+m3 = int(input("Mark of subject 3 : "))
+m4 = int(input("Mark of subject 4 : "))
+m5 = int(input("Mark of subject 5 : "))
+
+total_4 = m1+ m2+ m3+ m4+ m5
+print(total_4)
+
+avg_4 = total_4/5
+print(avg_4)
+print()
+print(f"student {e}")
+
+
+m1 = int(input("Mark of subject 1 : "))
+m2 = int(input("Mark of subject 2 : "))
+m3 = int(input("Mark of subject 3 : "))
+m4 = int(input("Mark of subject 4 : "))
+m5 = int(input("Mark of subject 5 : "))
+
+total_5 = m1+ m2+ m3+ m4+ m5
+print(total_5)
+
+avg_5 = total_5/5
+print(avg_5)
+
+name ="a,b,c,d,e"
+
+file = open("assignment.txt",'a')
+file = write('name')
+
+file =open("assignment.txt",'r')
+x = file
+print(x)
+
+
+# In[ ]:
+
+
+
+
