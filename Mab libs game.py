e7bc30de497151a548f220daa67f4c8a3a80cb8e
Adarsh220397 <79439872+Adarsh220397@users.noreply.github.com>
2021-09-27 10:25:22 +0530

    Add files via upload

---
+++ Mab libs game.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+color = input("Enter a color : ")
+plural_noun = input("Enter a Plural noun: ")
+celebrity = input("Enter a celebrity: ")
+print()
+print("Roses are " + color )
+print(plural_noun + "are blue")
+print("I love " + celebrity)
+
+
+# In[ ]:
+
+
+
+
